59095cef8ac9c343ac42755f869bab70e4f53229
gowthamaries <vgowthamp@gmail.com>
2021-12-29 08:54:42 +0530

    Third commit

--- aws elbv2 create-target-group --name ecs.sh
+++
@@ -1,3 +0,0 @@
-aws elbv2 create-target-group --name ecs-Store-$dtc_site_name --protocol HTTP --port 5000 --target-type ip --vpc-id vpc-09dea383b3c5cabf7
-
-aws elbv2 create-listener --load-balancer-arn $dtc_upper --protocol TCP --port 5000 --default-actions Type=forward,TargetGroupArn=$TARGET_NAME
